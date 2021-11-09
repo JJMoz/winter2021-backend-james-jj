f6faf7b687fbd2651b8945ad5a9ca20f46fa79aa
JJMoz <18526918+JJMoz@users.noreply.github.com>
2021-11-09 17:43:27 -0500

    heroku app set up

--- week3/web_server/index.js
+++ week3/web_server/index.js
@@ -4,10 +4,20 @@ const express = require("express");
 // creates webserver OBJECT and puts it in a variable "app"
 const app = express();
 
-// Allows Express to use the HTTP module of Node. Mkaes http module aware of "express"
+// Allows Express to use the HTTP module of Node. Makes http module aware of "express"
 const http = require("http").Server(app);
+
+// puts the port number in a variable so it is easier to change
+const port = 80;
 
 // tells the express/Http what port to use(listen too) 
 // 3000 is the most common port used in express
-http.listen(3000);
-console.log("Express server is running on port 3000."); 
+http.listen(port);
+// 'process.env is a Global variable s injected by the Node at runtime for your application to use and it represents the state of the system environment your application is in when it starts. For example, if the system has a PATH variable set, this will be made accessible to you through process.env.PATH which you can use to check where binaries are located and make external calls to them if required.
+//http.listen(process.env.PORT || 80);
+
+
+console.log("Express server is running on port " + port + "."); 
+
+// Express Routes. Allows us to "redirect" a URL filepath to a specific folder
+app.use("/", express.static("public_html/"));
