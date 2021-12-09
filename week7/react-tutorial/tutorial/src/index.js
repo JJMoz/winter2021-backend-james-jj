68a316e2f1a7e3396b738342296157ec1200df11
JJMoz <18526918+JJMoz@users.noreply.github.com>
2021-12-09 12:56:11 -0500

    X, O, game

--- week7/react-tutorial/tutorial/src/index.js
+++ week7/react-tutorial/tutorial/src/index.js
@@ -25,7 +25,7 @@ function Square(props) {
       <button 
       className="square" 
 
-        onClick= {props.onClick()}
+        onClick= {props.onClick}
       >
         {props.value}
       </button> 
@@ -43,22 +43,38 @@ class Board extends React.Component {
   }
 
   handleClick(i) {    
-    const squares = this.state.squares.slice();    
-    squares[i] = this.state.xIsNext ? 'X' : 'O';    
-    this.setState({squares: squares}), !this.state.xIsNext  
+    const squares = this.state.squares.slice(); 
+    
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? "X" : "O";    
+    this.setState({
+      squares: squares,
+      xIsNext:!this.state.xIsNext 
+     }); 
   }
 
 
   renderSquare(i) {
-    return 
+    return (
     <Square 
         value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
-      />;
+      />
+    );
 }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);    
+    let status;    
+    if (winner) {      
+      status = 'Winner: ' + winner;    } 
+      else {      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');    
+    }
+
+  
 
     return (
       <div>
@@ -106,7 +122,29 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
